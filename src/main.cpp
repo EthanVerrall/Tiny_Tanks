7b591788c6a7d459db85f5fb27da609eb0e411aa
Ethan Verrall <ejverrall24@gmail.com>
2026-02-28 15:46:20 +0000

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include "SFML/Graphics.hpp"
+
+int main() {
+
+	//Create default render window, not full screen, 8 levels of gpu anti aliasing
+	sf::ContextSettings settings;
+	settings.antiAliasingLevel = 8u;
+
+	//Here is the window
+	sf::RenderWindow window (sf::VideoMode({ 1200u,800u }), "Window", sf::Style::Default, sf::State::Windowed, settings);
+
+	//Create main window loop
+	while (window.isOpen()) {
+		
+		//Poll for events
+		while (std::optional<sf::Event> current_event = window.pollEvent()) {
+
+			//Top right exit button event, mouse click it
+			if (current_event->is<sf::Event::Closed> () ){
+
+				window.close();
+			}
+		}
+
+		//Clear every frame before drawing
+		window.clear(sf::Color::White);
+
+		/*
+		--------------------------
+		Draw below here with window.draw(my_object)
+		--------------------------
+		*/
+
+		//Displays everything drawn
+		window.display();
+	}
+
+	return 0;
+}
